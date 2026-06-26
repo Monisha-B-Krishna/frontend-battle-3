c5c5ea50693d3c30b8eaef2101bb1fce982b28d5
Monisha B K <monishabk2001@gmail.com>
2026-06-26 14:51:35 +0530

    add navbar

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Navbar scroll effect
+window.addEventListener('scroll', () => {
+  const nav = document.getElementById('mainNav');
+  if (window.scrollY > 50) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
